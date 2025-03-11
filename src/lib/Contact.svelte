92115a598cebc42b62858273c2f60c888867d883
Honzeenek <jan.palenik@icloud.com>
2025-03-11 18:51:32 +0100

    Finished frontend for contact page..

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -7,6 +7,7 @@
 </script>
 
 <main>
+    <div class="glow-effect"></div>
     <Nav />
     <section class="contact-content">
         <h1>Ready to enhance <br> your <span>WEB</span> game?</h1>
@@ -46,6 +47,29 @@
     main {
         padding: 0 5.5rem;
         position: relative;
+    }
+    .glow-effect {
+        position: absolute;
+        opacity: 50%;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 60vw;
+        height: 70px;
+        background: radial-gradient(
+                ellipse at center top,
+                rgba(255, 255, 255, 0.7) 0%,
+                rgba(255, 255, 255, 0.4) 20%,
+                rgba(255, 255, 255, 0) 60%
+        );
+        /*radial-gradient(*/
+        /*        ellipse at center top,*/
+        /*        rgb(70, 153, 255) 0%,*/
+        /*        rgba(70, 153, 255, 0.75) 20%,*/
+        /*        rgba(70, 153, 255, 0) 60%*/
+        /*);*/
+        pointer-events: none;
+        z-index: 1;
     }
 
 
